21c1056d685522316c3f44a4fe686ded03bed9b1
root <root@centos7ps.localdomain>
2015-03-16 23:22:55 -0400

    Change ofr branch

--- centos7.sh
+++ centos7.sh
@@ -1,55 +1,5 @@
 #!/bin/bash
 #Making sure that system is up-to-date
-cd /opt/perfectserver/
-date > servertime.txt
-echo This script will download all the depencencies to build php from source. depending on your system resources it could take up to 30 minutes.
-echo Relax and take a cup of coffee.
-sleep 10
-yum update -y
-#installing network tools (minimal install does not have it)
-yum install net-tools
-#installing development tools
-yum group install "Development Tools" -y
-#installing epel repo
-yum install epel-release -y
-#updating again the system
-yum update -y
-#install php depencencies
-yum install mariadb-server mariadb-client wget curl nano -y
-#install build depencencies for php
-yum install libmcrypt-devel bzip2-devel curl-devel db4-devel libjpeg-devel libpng-devel libXpm-devel gmp-devel libc-client-devel openldap-devel unixODBC-devel postgresql-devel sqlite-devel net-snmp-devel libxslt-devel pcre-devel mysql-devel postgresql-devel libxslt-devel mariadb-client freetype-devel.x86_64 -y
-#install httpd server to create APACHE USER TO RUN PHP-FPM
-#depencendies for nginx
-#yum install gcc gcc-c++ make zlib-devel pcre-devel openssl-devel -y
-yum install httpd -y 
-
-echo instalacion de dependencias
-date >> servertime.txt
-#uncompress 5.3 version.
-cd /opt/perfectserver/phpsources/
-tar xzvf php-5.5.3.tar.gz
-chmod 777 -R php-5.5.3/
-cd php-5.5.3/
-#configure php 5.3
-./configure --prefix=/opt/php53 --with-pdo-pgsql --with-zlib-dir --with-freetype-dir --enable-mbstring --with-libxml-dir=/usr --enable-soap --enable-calendar --with-curl --with-mcrypt --with-zlib --with-gd --with-pgsql --disable-rpath --enable-inline-optimization --with-bz2 --with-zlib --enable-sockets --enable-sysvsem --enable-sysvshm --enable-pcntl --enable-mbregex --enable-exif --enable-bcmath --with-mhash --enable-zip --with-pcre-regex --with-mysql --with-pdo-mysql --with-mysqli --with-jpeg-dir=/usr --with-png-dir=/usr --enable-gd-native-ttf --with-openssl --with-fpm-user=apache --with-fpm-group=apache --with-libdir=lib64     --enable-ftp --with-kerberos --with-gettext --with-xmlrpc --with-xsl --enable-opcache --enable-fpm
-# echo Php configure succesfull, now we are going to build it and install php 5.5.3
-make && make install
-#TODO CHANGE PORT
-mv /opt/php53/etc/php-fpm.conf.default /opt/php53/etc/php-fpm.conf 
-# echo Moving php fpm configuration to etc init folder
-# se configuro php y instalo en el directorio
-pwd
-cd sapi/fpm
-cp init.d.php-fpm /etc/init.d/php53
-#ls -al
-chmod 775 /etc/init.d/php53
-#informacion para modificar el archivo
-sed -i 's/^listen =.*/listen = 127.0.0.1:9053/' /opt/php53/etc/php-fpm.conf
-#listen = 127.0.0.1:9000
-#se instalo la version de php 5.3/
-cd /opt/perfectserver/
-echo php5.3 fin
-date >> servertime.txt
 
 
 cd /opt/perfectserver/phpsources/
@@ -75,6 +25,8 @@ sed -i 's/^listen =.*/listen = 127.0.0.1:9054/' /opt/php54/etc/php-fpm.conf
 cd /opt/perfectserver/
 echo php5.4 fin
 date >> servertime.txt
+echo se compilo php 5.4
+read time
 #########################################
 #build de php 5.5
 cd /opt/perfectserver/phpsources/
